c23203b4392e628d52f130978fd871c6e2260bc9
Garcias <sergio.garcia.sti@gmail.com>
2019-03-05 11:45:26 +0100

    añadido visualizadores de solicitudes

--- Practica-1/server.js
+++ Practica-1/server.js
@@ -4,6 +4,10 @@ console.log("Arrancando servidor...")
 
 
 http.createServer(function (req, res) {
+  console.log("--> Cabecera de la solicitud: ")
+    console.log("HOST: " + req.headers.host)
+  console.log("USER AGENT: " + req.headers['user-agent'])
+  console.log("Recurso solicitado (URL): " + req.url)
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
   console.log("Peticion atendida")
