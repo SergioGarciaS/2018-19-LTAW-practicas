94ac03e29f110c455f4c86f7b7517aba98623cdb
SergiogarciaS <garcias@sgs.localdomain>
2019-03-12 00:31:15 +0100

    envio de web principal con imagenes

--- Practica-1/server.js
+++ Practica-1/server.js
@@ -1,17 +1,38 @@
 var http = require('http');
 var fs = require ('fs');
+var path = require('path');
 console.log("Arrancando servidor...")
 
 
 http.createServer(function (req, res) {
   console.log("Recurso solicitado (URL): " + req.url)
 
-  if(req.url = '/'){
+  if(req.url == '/'){
     console.log("init");
     fs.readFile('index.html', (err,data) => {
       res.writeHead(200, {'Content-type': 'text/html'})
       res.end(data)
     })
+  }
+  if(req.url.split(".")[1] == 'jpg' || req.url.split(".")[1] == 'jpeg' ){
+    let dir_path = path.join(__dirname, req.url)
+    console.log(dir_path);
+    fs.readFile(dir_path, (err, data) => {
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' })
+        console.log("---> Peticion recibida")
+        console.log("Recurso solicitado (URL): " + req.url)
+    res.end(data)
+  })
+  }
+  if(req.url.split(".")[1] == 'png'){
+    let dir_path = path.join(__dirname, req.url)
+    console.log(dir_path);
+    fs.readFile(dir_path, (err, data) => {
+        res.writeHead(200, { 'Content-Type': 'image/png' })
+        console.log("---> Peticion recibida")
+        console.log("Recurso solicitado (URL): " + req.url)
+    res.end(data)
+  })
   }
 
   console.log("Peticion atendida")
