65a788e4ab1e5c1198fc722ac90aa010f73f8528
SergiogarciaS <garcias@sgs.localdomain>
2019-05-11 18:26:32 +0200

    Añadido numero de usuarios y reenvio de texto

--- Practica-4/test.js
+++ Practica-4/test.js
@@ -1,39 +1,55 @@
-var app = require('express')();
+var app = require('express')()
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-//--Servir la pagina principal
+//-- Puerto donde lanzar el servidor
+const PORT = 3000
+
+var clients_number = 0;
+
+//-- Punto de entrada pricipal
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-  console.log("Página principal2: /")
+  //res.send('Probando express... ¡¡¡qué fácil!!!')
+  console.log("Página principal: /")
+});
+
+//-- Servir el CSS
+app.get('/chat.css', function(req, res){
+  res.sendFile(__dirname + '/chat.css');
+  //res.send('Probando express... ¡¡¡qué fácil!!!')
+  console.log("Fichero de estilo solicitado")
 });
 
 //-- Servir el cliente javascript
 app.get('/chat-client.js', function(req, res){
   res.sendFile(__dirname + '/chat-client.js');
-  console.log("Fichero js solicituado")
+  //res.send('Probando express... ¡¡¡qué fácil!!!')
+  console.log("Fichero js solicitado")
 });
 
-//-- Lanzar el servidor
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+//-- Lanzar servidor
+http.listen(PORT, function(){
+  console.log('Servidor lanzado en puerto ' + PORT);
 });
 
 //-- Evento: Nueva conexion recibida
-//-- Un nuevo cliente se ha conectado!
-io.on('connection', function(socket){
-  console.log('--> Usuario conectado!');
+//-- Un nuevo cliente se ha conectado
 
+io.on('connection', function(socket){
+  console.log('--> Usuario conectado \r\n');
+  clients_number += 1;
+  console.log(clients_number);
+  socket.emit('new_message', 'Bienvenido amigo <br>');
+  socket.broadcast.emit('new_message', 'Hay un nuevo usuario, bienvenido!<br>');
   //-- Detectar si el usuario se ha desconectado
   socket.on('disconnect', function(){
-    console.log('--> Usuario Desconectado');
+    console.log('--> Usuario desconectado');
+    clients_number -= 1;
+    io.emit('new_message', 'Se ha desconectado un usuario<br>');
+    console.log(clients_number);
+
   });
-
-  //-- Detectar si se ha recibido un mensaje del cliente
-   socket.on('new_message', msg => {
-
-   //-- Notificarlo en la consola del servidor
-   console.log("Mensaje recibido: " + msg)
- });
+socket.on('new_message', msg => {io.emit('new_message', msg); })
 
 });
